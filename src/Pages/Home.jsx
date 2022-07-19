f35c56e189a1a5b7241b6d0f2255e7237b03cb04
RaviAppscrip7878 <kevadiyaravirameshbhai@appscrip.co>
2022-07-19 11:39:55 +0530

    Fix auth token

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,21 +2,20 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AlbumCard from "../Components/ListCard";
 import Loader from "../Components/Loader";
+import { getAuthTokenFromLocal } from "../Helpers/CommonHelpers";
 import {
   albumListSelector,
   getAlbumFetch,
   loaderSelector,
-  tokenSelector,
 } from "../Redux/ReducerSlices/PlayListSlice";
 
 export default function Home() {
   const dispatch = useDispatch();
   const albumList = useSelector(albumListSelector);
   const isLoading = useSelector(loaderSelector);
-  const token = useSelector(tokenSelector);
+  const token = getAuthTokenFromLocal();
 
   useEffect(() => {
-    console.log("token", token);
     if (albumList.length <= 0 && token !== null) {
       dispatch(getAlbumFetch());
     }
